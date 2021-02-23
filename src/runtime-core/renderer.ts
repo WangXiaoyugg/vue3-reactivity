95d36f41ad1777c26a6149000d67ea6fe0749f4a
xywang71 <xywang71@iflytek.com>
2021-02-23 11:22:53 +0800

    feat: diff算法-01

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -19,8 +19,18 @@ function baseCreateRenderer(options) {
        patch(null, vnode, container)
     }
 
+    const isSameVnodeType = (n1, n2) => {
+        return n1.type === n2.type && n1.key === n2.key;
+    }
+
     const patch = (n1, n2, container) => {
         let { shapeFlag } = n2
+
+        // 老节点和新节点不相同
+        if (n1 && !isSameVnodeType(n1, n2)) {
+            hostRemove(n1.el)
+            n1 = null
+        } 
 
         if (shapeFlag & ShapeFlags.ELEMENT) {
             processElement(n1, n2, container)
@@ -55,8 +65,80 @@ function baseCreateRenderer(options) {
            patch(null, children[i], container) 
         }
     }
+
+    const patchProps = (oldProps, newProps, el) => {
+        if (oldProps !== newProps) {
+            // 新的需要覆盖老的
+            for (let key in newProps) {
+                const prev = oldProps[key];
+                const next = newProps[key];
+                if (prev !== next) {
+                    hostPatchProp(el, key, prev, next)
+                }
+            }
+
+            // 老的有的新的没有，将老的删除掉
+            for (let key in oldProps) {
+                if (!(key in newProps)) {
+                    hostPatchProp(el, key, oldProps[key], null)
+                }
+            }
+
+            
+        }
+    }
     
-    const patchElement = (n1, n2, container) => {}
+    const patchElement = (n1, n2, container) => {
+        // 如何 n1 和 n2 的类型一样，复用老节点，直接改属性
+       let el = (n2.el = n1.el)
+       const oldProps = n1.props || {};
+       const newProps = n2.props || {};
+
+       // 比对前后的属性差异
+       patchProps(oldProps, newProps, el); 
+
+       // 比较子节点
+       patchChildren(n1, n2, el);
+
+    }
+
+    const patchChildren = (n1, n2, el) =>  {
+        // 获取所有老的节点
+        const c1 = n1.children;
+        // 获取所有新的节点
+        const c2 = n2.children;
+        const prevShapeFlag = n1.shapeFlag
+        const shapeFlag = n2.shapeFlag
+
+        if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+            // 老的是文本，新的是文本， 新的替换老的
+            // 老的是数组，新的是文本， 覆盖老的即可
+            if (c2 !== c1) {
+                hostSetELementText(el, c2)
+            }
+        } else {
+            // 新的是数组, 老的也是数组，diff算法
+            if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+                console.log('diff 算法')
+            } else {
+            // 老的是文本，新的是数组， 移除老的文本，生成新的节点塞进去
+                if (prevShapeFlag & ShapeFlags.TEXT_CHILDREN) {
+                    hostSetELementText(el, '');
+                }
+                if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+                    // 去把新的元素进行挂载
+                    for (let i = 0; i < c2.length; i++) {
+                        patch(null, c2[i], el);
+                    }
+                }
+            }
+        } 
+
+        
+        // 老的是数组，新的是数组， diff算法
+
+    }
+
 
     const mountComponent = (initialVnode, container) => {
        const instance = initialVnode.component = createComponentInstance(initialVnode)
@@ -77,6 +159,7 @@ function baseCreateRenderer(options) {
                 let prev = instance.subTree;
                 let next = instance.render();
                 console.log(prev, next);
+                patch(prev, next, container)
             }
         })
     }
